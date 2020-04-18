9cf6ce46d37a3ac9a60e4c91a716b0b51b4482e3
wilmir <wbnicanor01@gmail.com>
2020-04-18 08:26:41 +0100

    Updates HTTP request to async await, Adds the sign up and login for front end

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 const resultContainer = document.querySelector('.result-container');
 const loadPeopleBtn = document.querySelector('.load-people');
 
-function loadPeople(){
-    fetch('http://localhost:8080/messenger/webapi/profiles')
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-        data.forEach(function(person){
-            const div = document.createElement('div');
-            div.innerHTML = person.firstName;
-            resultContainer.appendChild(div);
-        })
+async function loadPeople(){
+    const url = 'http://localhost:8080/messenger/webapi/profiles';
+
+    try{
+        const response = await fetch(url);
+
+        if(response.ok){
+            const jsonResponse = await response.json();
+    
+            renderResponse(jsonResponse);
+
+            console.log(jsonResponse);
+        }else{
+            throw new Error("Request failed");
+        }    
+
+    }catch(error){
+        console.log(error)
+    }
+}
+
+
+function renderResponse(jsonResponse){
+    jsonResponse.forEach(person => {
+        const div = document.createElement('div');
+        div.innerHTML = `<div> Name: ${person.firstName} ${person.lastName}</div>`;
+        resultContainer.appendChild(div);
     });
 }
+
 
 loadPeopleBtn.addEventListener('click', function(){
     resultContainer.innerHTML = '';
